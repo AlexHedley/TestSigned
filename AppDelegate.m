623d13e163076293aaf12a36aa047293b5608ed5
Alex Hedley <alex.hedley@me.com>
2016-11-27 10:43:23 +0000

    Adding App Delegate

---
+++ AppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.m
+//
+//  Created by Alexander Hedley on 27/11/2016.
+//  Copyright (c) 2016 Alex Hedley. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)application {
+    return YES;
+}
+
+@end
